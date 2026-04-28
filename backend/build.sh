60089e293ad2829778086cc608a449dc9d10bf0d
luisbastosGit <luisbastos2@gmail.com>
2026-04-28 20:49:00 -0300

    Update Chromium installation command in build script
    Removed '--with-deps' option to prevent root permission issues.

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# NOVO CÓDIGO INSERIDO AQUI - 28/04/2026 20:41
+# NOVO CÓDIGO INSERIDO AQUI - 28/04/2026 20:49
 
 # Interrompe a execução caso ocorra algum erro
 set -o errexit
@@ -7,5 +7,6 @@ set -o errexit
 # 1. Instala as bibliotecas do Python
 pip install -r requirements.txt
 
-# 2. Baixa e instala o navegador Chromium e suas dependências no servidor
-playwright install chromium --with-deps
+# 2. Baixa e instala APENAS o navegador Chromium no nível do usuário
+# A remoção do '--with-deps' evita o bloqueio de permissão root (sudo) do Render
+playwright install chromium
